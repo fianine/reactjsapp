fb83ca59d6408bdf25f4673653ae1a67e3a387dc
fianine <princealfiyana@gmail.com>
2017-06-11 21:44:46 +0700

    Todo App 2017.06.11

---
+++ script.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {TodoApp} from './src/TodoApp';
+
+ReactDOM.render(
+  <TodoApp />,
+  document.getElementById('root')
+);
---
+++ src/TodoApp.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+class TodoApp extends React.Component {
+
+}
+
+export default TodoApp;
